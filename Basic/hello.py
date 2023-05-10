3e73015ab023f9405ac09d145fca72f6c6cbffbb
Guillermo <guillegarzon.gg@gmail.com>
2023-05-10 16:28:17 -0500

    feat:Hello world and data Types

---
+++ Basic/hello.py
@@ -0,0 +1,20 @@
+# Esto es un comentario:
+# Hola Mundo en Python
+
+print('Hola Python')
+print("Hola Python")
+
+"""
+Este es un comentario en 
+varias
+líneas
+"""
+
+# Consultar el tipo de dato
+print(type("Soy un dato str")) # Tipo 'str'
+print(type(5)) # Tipo 'int'
+print(type(1.5)) # Tipo 'float'
+print(type(3 + 1j)) # Tipo 'complex'
+print(type(True)) # Tipo 'bool'
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType'
+
